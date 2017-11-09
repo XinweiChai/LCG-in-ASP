ed4ab0d18f89a29671eb0db01b8c413c34608e1d
Xinwei Chai <xinwei.chai@hotmail.com>
2017-11-09 17:07:56 +0100

    possible sequences

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LCG-in-ASP
 
 The continuation of PermReach
+
+This program is aimed to solve the problem of big AND gates(say more than 5), which used to lead to heavy computation. To avoid global searching, this ASP-based approach gives possible firing order of local states in a simple AND gate, which permits the reachability of conjoint transition.
--- priority.pl
+++ priority.pl
@@ -1,13 +1,13 @@
 %prior(B1,B2) :- nodeState(B1,_,X,A), nodeState(B2,_,X,_), proc(X), init(X,A), B1!=B2.%priority of branches
 %---------------
 %priority of states
-node(B,N) :- branch(B), nodeState(B,N,_,_).
-prior(P1,S1,P2,S2) :- nodeState(B,N1,P1,S1), nodeState(B,N2,P2,S2), N1>N2. 
-prior(P,S1,P,S2) :- nodeState(B1,N1,P,S1), nodeState(B2,N2,P,S2), init(P,S1), S1!=S2.
-prior(P3,S3,P,S2) :- nodeState(B1,N1,P,S1), nodeState(B2,N2,P,S2), init(P,S1), S1!=S2, nodeState(B1,N1-1,P3,S3).
-prior(P1,S1,P3,S3) :- prior(P1,S1,P2,S2), prior(P2,S2,P3,S3).
+%node(B,N) :- branch(B), nodeState(B,N,_,_).%Definition with only positions
+prior(P1,S1,P2,S2) :- nodeState(B,N1,P1,S1), nodeState(B,N2,P2,S2), N1>N2.%If one node is after another, then it is fired at first 
+prior(P,S1,P,S2) :- nodeState(B1,N1,P,S1), nodeState(B2,N2,P,S2), init(P,S1), S1!=S2.%If a node is in initial state, then it is prior to others
+prior(P3,S3,P,S2) :- nodeState(B1,N1,P,S1), nodeState(B2,N2,P,S2), init(P,S1), S1!=S2, nodeState(B1,N1-1,P3,S3).%and the predecessor is also prior
+prior(P1,S1,P3,S3) :- prior(P1,S1,P2,S2), prior(P2,S2,P3,S3).%Passivity of priority
 localState(P,S) :- nodeState(_,_,P,S).
-1{seq(1..O,P,S)}1 :- localState(P,S), O={node(B,N):nodeState(B,N,_,_)}.
+1{seq(1..O,P,S)}1 :- localState(P,S), O={nodeState(B,N,P1,S1):nodeState(B,N,P1,S1)}.
 :- prior(P1,S1,P2,S2), seq(O1,P1,S1), seq(O2,P2,S2),O1>O2.
 :- seq(O1,P1,S1), seq(O2,P2,S2), P1!=P2, O1=O2.
 :- seq(O1,P1,S1), seq(O2,P2,S2), S1!=S2, O1=O2.
