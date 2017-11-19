da49300b6627337916bf2ed0cbde1b2d8e83c2fa
Xinwei Chai <xinwei.chai@hotmail.com>
2017-11-20 00:29:49 +0100

    Nested AND Gates

--- LCGexample6.pl
+++ LCGexample6.pl
@@ -3,12 +3,14 @@ proc(b).
 proc(c).
 proc(d).
 proc(e).
+proc(f).
 
 init(a,0).
 init(b,0).
 init(c,0).
 init(d,0).
 init(e,0).
+init(f,0).
 
 node(a,1,1).
 node(b,1,2).
@@ -16,7 +18,7 @@ node(c,1,3).
 node(e,1,4).
 node(d,0,5).
 node(e,0,6).
-node(d,1,7).
+node(f,0,7).
 node(a,0,8).
 
 parent(1,2).
@@ -28,5 +30,4 @@ parent(3,7).
 parent(4,8).
 parent(5,8).
 parent(6,8).
-parent(7,8).
 
--- nested.pl
+++ nested.pl
@@ -1,4 +1,16 @@
 %---N stands for node numbering
 prior(N1,N2) :- parent(N2,N1).
 prior(N1,N3) :- prior(N1,N2), prior(N2,N3).
-prior(N1,N2) :- node(P1,S1,N1), node(P2,S2,N2), node(P2,S3,N3), parent(N1,N3), init(P2,S3), S2!=S3.
+prior(N1,N2) :- node(P1,S1,N1), node(P2,S2,N2), node(P2,S3,N3), parent(N1,N3), init(P2,S3), S2!=S3, P1!=P2.
+%localState(P,S) :- node(P,S,N).
+%1{seq(1..O,P,S)}1 :- localState(P,S), O={node(P1,S1,N1):node(P1,S1,N1)}.
+1{seq(1..O,P,S)}1 :- node(P,S,N), O={node(P1,S1,N1):node(P1,S1,N1)}.
+:- prior(N1,N2), node(P1,S1,N1), node(P2,S2,N2), seq(O1,P1,S1), seq(O2,P2,S2), O1>O2.
+:- seq(O1,P1,S1), seq(O2,P2,S2), P1!=P2, O1=O2.
+:- seq(O1,P1,S1), seq(O2,P2,S2), S1!=S2, O1=O2.
+%--------
+%simplification of output
+:- seq(O1,P1,S1), seq(O2,P2,S2), init(P1,S1), not init(P2,S2),O1>O2.
+:- seq(O1,P1,S1), seq(O2,P2,S2), init(P1,S1), init(P2,S2), P1<P2,O1>O2.
+
+#show seq/3.
