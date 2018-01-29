2e1a102b12bf1362772fcd4efc86a07225671766
Xinwei Chai <xinwei.chai@hotmail.com>
2018-01-29 18:14:53 +0100

    parser finish

--- SLCG.py
+++ SLCG.py
@@ -1,35 +1,19 @@
-def SLCG(path):
-    readProc = 0
-    hasStartNode = 0
-    process = []
-    dictionary = []
-    actions = []
-    f = open('data/'+path,'r')
-    for line in f.readlines():
-        if 'goal' in line
-            hasStartNode = 1
-            words = re.split(",*\s|\(\*|\*\)",line)
-            startState[words.index('goal')+1]
-            startState = re.split("_|='",startState)
-        else 
-            words = re.split("\s*",line)
-            if len(words) <= 1
-                continue
-            elif (len(words) > 2) and (not readProc)
-                readProc = 1
-                dictionary.append(process)
-                #if hasStartNode
-                   # startState = [dictionary[], ]
-            if not readProc
-                process.append(words[0])
+def SLCG(initialState, actions, startNode):
+    lcg={}
+    #processdictionary
+    ls = [startNode]
+    LS = set([startNode])
+    while ls
+        for i in ls
+            if i in LS and [i] != LS
+                Ls.pop(0)
+                continue;
+            LS = LS|set([i])
+            ls.pop()
+            if i[1] == initialState[i[0]]
                 continue
             else
-                initialState = np.zeros(len(process))
-                if not words[0] == 'initial_state'
-                    act = []
-                else
-                    for i in range(4)
-                        temp = re.split(",*\s",line)
-                        initialState 
-    return dictionary,actions,initialState,startState 
-                
+                nodei = stateNodeArray...
+                act = target
+
+    return stateNodeArray, lcg, solNodeArray
---
+++ readBAN.py
@@ -0,0 +1,42 @@
+def readBAN(path):
+    readProc = 0
+    hasStartNode = 0
+    process = []
+    dictionary = []
+    actions = []
+    f = open('data/'+path,'r')
+    for line in f.readlines():
+        if 'goal' in line
+            hasStartNode = 1
+            words = re.split(",*\s|\(\*|\*\)",line)
+            startState[words.index('goal')+1]
+            startState = re.split("_|='",startState)
+        else 
+            words = re.split(",*\s*",line)
+            if len(words) <= 1
+                continue
+            elif (len(words) > 2) and (not readProc)
+                readProc = 1
+                dictionary.append(process)
+                #if hasStartNode
+                   # startState = [dictionary[], ]
+            if not readProc
+                process.append(words[0])
+                continue
+            else
+                initialState = np.zeros(len(process))
+                if words[0] != 'initial_state'
+                    act=[[]]
+                    act.append(words[0])
+                    act.append(words[1])
+                    act.append(words[3])
+                    for i in range(len(words)-5)
+                        temp = re.split("=",words[i+5]) #last elements
+                       act[0].append([temp[0],temp[1]])
+                    actions.append(act)
+                else
+                    for i in range(1,len(words))
+                        temp = re.split("=",line)
+                        initialState[process.index(temp[0])] = temp[1]
+    return dictionary,actions,initialState,startState 
+                
