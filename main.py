456ce0f2a8da4fc7b15bef2423beee3283a17149
Xinwei Chai <xinwei.chai@hotmail.com>
2018-03-30 15:33:02 +0900

    command line

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from SLCG import *
 from cycle import *
 from precodition import *
 from heuristics import *
-[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('tcrsig94.an')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("fn")
+parser.add_argument("init")
+args=parser.parse_args()
+[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN(args.fn)
+startNode=tuple(re.split('=',args.init))
+#[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('tcrsig94.an')
 #[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('OrTest.lp')
 #[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('LCG5')
 #[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('loopTest.lp')
--- testmain.py
+++ testmain.py
@@ -1,40 +1,14 @@
-#from __future__ import print_function
-from pyasp.asp import *
-from SCC import strongly_connected_components_path
-#input
-#tentatives on translation from Matlab to Python
-#stateNodeArray adjMatrix solNodeArray
-#initialState actions startNode
-f=open('data/LCG1','r')
-for line in f.readlines():
-    line.split(" ")
-    #print(line)
-
-sccResult=[]
-#for scc in strongly_connected_components_path(vertices, edges):
-#    sccResult.append(scc)
-#preconditioning
-#adjacent list perhaps faster
-#while SCC is valid, break cycle
-
-#begin for
-#from root node, choose arbitrarily
-
-#a possible alternative for ASP part is to enumerate all the possible combinations of OR gates
-#write fact file
-
-goptions = ''
-soptions = '1'
-solver   = Gringo4Clasp(gringo_options=goptions, clasp_options=soptions)
-encoding = 'nestedTest.lp'
-facts    = 'LCGexample5.pl'
-result   = solver.run([encoding, facts], collapseTerms=True, collapseAtoms=False)
-for s in result : 
-    for a in s :
-        #if 'reachable' in a or 'reachableAfterAddition' in a:
-        if a.pred()=='reachableAfterAddition':
-            print(1)
-#    args= ",".join(a.args())
-#    print(a.pred(), '(',args, ')', sep='', end=' ')
-#  print()
-#end for
+from readBAN import *
+from SLCG import *
+from cycle import *
+from precodition import *
+from heuristics import *
+[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('tcrsig94.an')
+#[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('OrTest.lp')
+#[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('LCG5')
+#[dictionary,actions, actionsByHitter, initialState, startNode]=readBAN('loopTest.lp')
+[lcgNodes, lcgEdges] = SLCG(initialState, actions, startNode)
+lcgEdges=cycle(lcgNodes,lcgEdges, startNode, actionsByHitter, actions)
+lcgEdges = precondition(lcgEdges, actionsByHitter,initialState)
+iteration=500
+heuristics(iteration,lcgEdges,startNode,initialState,lcgNodes)
