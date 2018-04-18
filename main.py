831bb312315e21326a367097730585984281fadb
Xinwei Chai <xinwei.chai@hotmail.com>
2018-04-18 16:48:52 +0900

    inconclusive test on phagelambda

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import timeit
 #import argparse
 start = timeit.default_timer()
 #batch('data_tcr','tcrsig94.an')
-batch('data_egfr','egfr104.an')
+#batch('data_egfr','egfr104.an')
+batch('data_phage','testPhage')
+
 stop = timeit.default_timer()
 
 print(stop - start)
