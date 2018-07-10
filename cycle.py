bc76ea8168db32683b052e2b627bd35bffdea055
Xinwei Chai <xinwei.chai@hotmail.com>
2018-07-10 19:35:29 +0200

    cycles updated

--- cycle.py
+++ cycle.py
@@ -8,7 +8,6 @@ def break_cycle(lcg_edges, scc, start_node, actions_by_hitter, actions):
         for i in scc:
             if len(i) == 2:
                 for j in actions_by_hitter[i]:
-                    # for k in j[0]:
                     for k in (j[1], j[3]):
                         if k not in scc:
                             lcg_edges = delete_element(i, scc, lcg_edges, actions, actions_by_hitter)
@@ -19,17 +18,8 @@ def break_cycle(lcg_edges, scc, start_node, actions_by_hitter, actions):
 def delete_element(element, scc, lcg_edges, actions, actions_by_hitter):
     for i in scc:
         if i in actions_by_hitter[element]:
-            # lcg_edges[i].remove(element)
             lcg_edges[i] = []
             actions[(i[1], i[3])].remove(i)
-            # for j in scc:
-            #    if len(j)==2:
-            #        if i in actions[j]:
-            #            lcg_edges[j].remove(i)
-
-    # lcg_edges[actions_by_hitter[element]] = []  # delete the link of preceding action->element
-    # lcg_edges[(
-    # actions[element][1], actions[element][3])] = []  # delete the link of pred of preceding action -> preceding action
     return lcg_edges
 
 
@@ -44,4 +34,29 @@ def cycle(lcg_nodes, lcg_edges, start_node, actions_by_hitter, actions):
                 lcg_edges = break_cycle(lcg_edges, scc, start_node, actions_by_hitter, actions)
                 break
     return lcg_edges
-# def deleteExcessive
+
+
+def cycle2(lcg_nodes, lcg_edges, start_node, actions_by_hitter, actions):
+    cycle = True
+    while cycle:
+        cycle = False
+        SCC = strongly_connected_components_path(lcg_nodes, lcg_edges)
+        single_incoming = []
+        for i in SCC:
+            count_incoming = 0
+            for j in i:
+                contain = False
+                for k in actions_by_hitter[j]:
+                    for m in k:
+                        if m not in SCC:
+                            contain = True
+                if contain:
+                    count_incoming = count_incoming + 1
+            if count_incoming <= 1:
+                single_incoming.append(i)
+        for scc in single_incoming:
+            if len(scc) > 1:
+                cycle = True
+                lcg_edges = break_cycle(lcg_edges, scc, start_node, actions_by_hitter, actions)
+                break
+    return lcg_edges
